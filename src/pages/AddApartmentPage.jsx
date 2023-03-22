46c581da842d2e446696df91e3f2a1e9cabce980
Matias Molina Bishop <molibi01@gmail.com>
2023-03-22 14:14:45 +0100

    finished sample

--- src/pages/AddApartmentPage.jsx
+++ src/pages/AddApartmentPage.jsx
@@ -1,7 +1,62 @@
+import axios from "axios";
+import { useState } from "react";  
+import { useNavigate } from "react-router-dom"
+
 function AddApartmentPage() {
+
+    const [headline, setHeadline] = useState('')
+    const [price, setPrice] = useState(0)
+    const [img, setImg] = useState('')
+
+    //React Router’s useNavigate() hook returns a function that lets you navigate programmatically.
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const body = {title:headline, pricePerDay: price, img:img}
+
+        try{
+
+            await axios.post("https://ironbnb-m3.herokuapp.com/apartments", body)
+            setHeadline('')
+            setPrice(0)
+            setImg('')
+            navigate('/');  
+
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return (
       <div className="AddApartmentPage">
         <h3>Add New Apartment</h3>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input 
+                type='text'
+                name='headLine'
+                value={headline}
+                onChange={(e) => setHeadline(e.target.value)}
+            />
+            <label>Price</label>
+            <input 
+                type='text'
+                name='price'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+            />
+            <label>Image URL</label>
+            <input
+                type='text'
+                name='img'
+                value={img}
+                onChange={(e) => setImg(e.target.value)}
+            >
+            </input>
+            <button type='submit'>Create Apartment</button>
+        </form>
       </div>
     );
   }
--- src/pages/ApartmentsPage.jsx
+++ src/pages/ApartmentsPage.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
  
 function ApartmentsPage() {
   const [apartments, setApartments] = useState([]);
+
+  const APIurl = "https://ironbnb-m3.herokuapp.com/apartments"
+
+  const fetchApartments = async() => {
+    try{
+        const response  = await axios.get(APIurl)
+        console.log(response.data)
+        setApartments(response.data)
+        console.log('THIS IS THE APARTMENST LIST:', apartments)
+
+    }catch(err){
+        console.log(err)
+    }
+  }
+
+useEffect(() => {
+    fetchApartments()
+}, [])
   
   return (
     <div>
       <h3>List of apartments</h3>
+      {apartments.map((apartment) => (
+        <div key={apartment._id} className="card">
+          <img src={apartment.img} alt="apartment" />
+          <h3>{apartment.title}</h3>
+          <p>Price: {apartment.pricePerDay}</p>
+        </div>
+      ))}
     </div>
   );
 }
